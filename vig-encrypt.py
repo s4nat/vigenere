76f737f666c587f1f09a5ab71f4630987e4fb77d
uosjapuelks <anderson@u.nus.edu>
2023-01-23 15:00:51 +0100

    turned into function

--- vig-encrypt.py
+++ vig-encrypt.py
@@ -1,34 +1,33 @@
-alphalist = "abcdefghijklmnopqrstuvwxyzåäöabcdefghijklmnopqrstuvwxyzåäö"
-alphadict = {}
-keyshift = {}
+def vig_encrypt(plaintext,key):
+    alphalist = "abcdefghijklmnopqrstuvwxyzåäö"
+    alphadict = {}
+    keyshift = {}
 
-for i in range(29):
-    alphadict[i] = alphalist[i]
-    keyshift[alphalist[i]] = i
+    for i in range(29):
+        alphadict[i] = alphalist[i]
+        keyshift[alphalist[i]] = i
 
-print(alphadict)
-print(keyshift)
+    def getIndex(text, pointer):
+        return keyshift[text[pointer]]
 
-plaintext="iloveuppsala"
-key = "jöhn"
+    ptr = 0
+    ciphertextls = []
+    keyptr = -1
 
-ptr = 0
-ciphertextls = []
-keyptr = -1
+    while ptr < len(plaintext):
+        keyptr+=1
+        keyptr=keyptr%len(key)
 
-def getIndex(text, pointer):
-    return keyshift[text[pointer]]
+        index = getIndex(plaintext,ptr) + getIndex(key,keyptr)
+        index = index%29
+        cipherletter = alphadict[index]
+        ciphertextls.append(cipherletter)
+        ptr += 1
 
-print(getIndex(plaintext, 8))
+    return("".join(ciphertextls))
 
-while ptr < len(plaintext):
-    keyptr+=1
-    keyptr=keyptr%len(key)
+pt="iloveuppsala"
+k = "jöhn"
 
-    index = getIndex(plaintext,ptr) + getIndex(key,keyptr)
-    index = index%29
-    cipherletter = alphadict[index]
-    ciphertextls.append(cipherletter)
-    ptr += 1
-
-print("".join(ciphertextls))
+ct = vig_encrypt(pt, k)
+print(ct)
